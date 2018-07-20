7a5fcdafd50651f2fac57bdd63c2862a62ffa8e8
Will <will.l.zimmerman@gmail.com>
2018-07-20 13:14:46 -0400

    initial commmit

---
+++ src/TwoSum.java
@@ -0,0 +1,12 @@
+public class TwoSum {
+
+    int num[] = {2,7,11,15};
+    int target = 9;
+    public int[] twoSum(int[] nums, int target) {
+        if (nums[0] + nums[1] == target){
+           return new int[]{nums[0]};
+
+        }
+        return nums;
+    }
+}
